13dd370285ba4d7347772054613694fecf0d3b69
HelioJr548 <heliojr548@gmail.com>
2023-07-25 12:29:39 -0300

    :tada: Commit inicial

---
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -0,0 +1,18 @@
+# Declara duas variáveis, nome e idade, e atribui os valores "Helio" e 20, respectivamente.
+nome = "Helio"
+idade = 20
+
+# Troca os valores das variáveis nome e idade.
+nome, idade = "Junior", 21
+
+# Imprime os valores das variáveis nome e idade.
+print(nome, idade)
+
+# Declara uma variável, limite_saque_diario, e atribui o valor 1000.
+limite_saque_diario = 1000
+
+# Declara uma lista, BRAZILIAN_STATES, e atribui os valores "SP", "RJ", "SC", e "RS".
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+# Imprime a lista BRAZILIAN_STATES.
+print(BRAZILIAN_STATES)
