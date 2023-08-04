a82ea783631de3a8ccd9e2de431f3b08273be40a
HelioJr548 <heliojr548@gmail.com>
2023-08-04 08:53:37 -0300

    :tada: Conteudo sobre dicionarios

---
+++ 01 - Estrutura de Dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -0,0 +1,8 @@
+pessoa = {"nome": "Helio", "idade": 20}
+print(pessoa)
+
+pessoa = dict(nome="Helio", idade=20)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # {"nome": "Helio", "idade": 20, "telefone": "3333-1234"}
+print(pessoa)
